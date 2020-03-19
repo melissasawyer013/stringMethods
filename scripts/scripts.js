eedfb34873acf5a02dc9b1f70941a73f5b242e52
Melissa Sawyer <melissasawyer013@gmail.com>
2020-03-19 12:22:21 -0400

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,91 @@
+// I commented out each problem before working on the next one, so you just have to uncomment the problem you want to see. This was fun!
+
+
+// Problem 1
+
+// let coffee = "French";
+// let flavor = "Vanilla";
+// let order = coffee.concat(` `,flavor);
+// console.log(order);
+
+// or
+
+// let coffee = "French ";
+// let flavor = "Vanilla";
+// let order = coffee.concat(flavor);
+// console.log(order);
+
+
+
+// Problem 2
+
+// let coffee = 3;
+// let chai = 10;
+// console.log(`I have ${coffee} coffees.`);
+// console.log(`I also have ${chai} chais.`)
+// console.log(`I have a total of ${coffee + chai} drinks total.`)
+
+
+
+// Problem 3 Part 1
+
+// function myFunction () {
+//     let str = "Can you guess what is the index of 42 is? I am sure you will be surprised!";
+//     let answer = str.indexOf("42");
+//     console.log(answer);
+// }
+// myFunction();
+
+
+
+// Problem 3 Part 2
+
+// function myFunction () {
+//     let str = "Can you guess what is the index of 42 is? I am sure you will be surprised!";
+//     let answer = str.length;
+//     console.log(answer);
+// }
+// myFunction();
+
+
+
+// Problem 4
+
+// function myFunction () {
+//     let str1 = "How you doin?";
+//     let str2 = str1.replace ("doin", "doing");
+//     console.log(str2);
+// }
+// myFunction();
+
+
+
+// Problem 5
+
+// function myFunction() {
+//     let str = "         SOCIAL DISTANCING!!!                    ";
+//     alert (str.trim());
+// }
+// myFunction();
+
+
+
+// Problem 6 Part 1
+
+// function myFunction() {
+//     let str = "social distancing!!";
+//     let res = str.toUpperCase();
+//     console.log (res);
+// }
+// myFunction();
+
+
+
+// Problem 6 Part 2
+
+// function myFunction() {
+//     let str = "QuiEt VoICeS...PLEASE";
+//     let res = str.toLowerCase();
+//     console.log (res);
+// }
+// myFunction();
